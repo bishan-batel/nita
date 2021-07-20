4a06efa1fd6b927fae073955438f8b61eed271cb
Kishan <bishan.batel@protonmail.com>
2021-07-20 00:52:57 -0500

    Removed unrequired reference to object that calls Dispatch

--- src/utils/Timeout.cs
+++ src/utils/Timeout.cs
@@ -10,32 +10,28 @@ namespace Parry2.utils
         /// <summary>
         ///     Calls action after set time using godot's timer node
         /// </summary>
-        /// <param name="disposeWith">Godot object that the action is linked too</param>
+        /// <param name="node">Godot object that the action is linked too</param>
         /// <param name="action">Action to be called once time is over</param>
         /// <param name="delay">Delay for action in seconds</param>
-        // [Obsolete("Use GodotRX WaitFor instead", true)]
-        public static void Dispatch(this Node disposeWith, Action action, float delay)
+        public static void Dispatch(this Node node, Action action, float delay)
         {
-            disposeWith.AddChild(new TimeoutTimer(disposeWith, action, delay));
+            node.AddChild(new TimeoutTimer(action, delay));
         }
 
-        // [Obsolete("Use GodotRX WaitFor instead", true)]
         class TimeoutTimer : Node
         {
             readonly Action _action;
             readonly float _delay;
-            Object _disposeWith;
             Timer _timer;
 
-            public TimeoutTimer() : this(null, null, 0)
+            public TimeoutTimer() : this(null, 0)
             {
             }
 
-            public TimeoutTimer(Object disposeWith, Action action, float delay = 0)
+            public TimeoutTimer(Action action, float delay = 0)
             {
                 _action = action ?? (() => { });
                 _delay = delay;
-                _disposeWith = disposeWith;
             }
 
             public override void _Ready()
