9f0294679ca3fb40bc7003c075cb2bdfead4522c
Kishan Patel <bishan.batel@protonmail.com>
2021-08-22 13:37:53 -0500

    ?

--- src/managers/InputManager.cs
+++ src/managers/InputManager.cs
@@ -6,53 +6,53 @@ namespace Nita.managers
 {
   public static class InputManager
   {
-    public static InputController Apply(InputController controller)
-    {
-      var walkInput = new Vector2(
-        Input.GetActionStrength("right") - Input.GetActionStrength("left"),
-        Input.GetActionStrength("down") - Input.GetActionStrength("up")
-      );
+	public static InputController Apply(InputController controller)
+	{
+	  var walkInput = new Vector2(
+		Input.GetActionStrength("right") - Input.GetActionStrength("left"),
+		Input.GetActionStrength("down") - Input.GetActionStrength("up")
+	  );
 
-      return new InputController
-      {
-        JumpPressed = Input.IsActionPressed("jump"),
-        JumpJustPressed = Input.IsActionJustPressed("jump"),
+	  return new InputController
+	  {
+		JumpPressed = Input.IsActionPressed("jump"),
+		JumpJustPressed = Input.IsActionJustPressed("jump"),
 
-        AttackRotation = GetAttackRot(controller.Owner),
-        // AttackRotation = walkInput.Angle() + Mathf.Pi,
-        AttackJustPressed = Input.IsActionJustPressed("attack"),
-        WalkInput = walkInput,
+		AttackRotation = GetAttackRot(controller.Owner),
+		// AttackRotation = walkInput.Angle() + Mathf.Pi,
+		AttackJustPressed = Input.IsActionJustPressed("attack"),
+		WalkInput = walkInput,
 
-        Owner = controller.Owner
-      };
-    }
+		Owner = controller.Owner
+	  };
+	}
 
-    static float GetAttackRot(Node2D controller)
-    {
-      float GetMouseRotation()
-      {
-        // if (controller is null) return 0f;
-        float angle = controller.GetLocalMousePosition().Angle();
-        GD.Print(angle);
-        return angle;
-      }
+	static float GetAttackRot(Node2D controller)
+	{
+	  float GetMouseRotation()
+	  {
+		// if (controller is null) return 0f;
+		float angle = controller.GetLocalMousePosition().Angle();
+		GD.Print(angle);
+		return angle;
+	  }
 
-      float GetControllerRotation()
-      {
-        float angle = new Vector2(
-          Input.GetJoyAxis(0, (int)JoystickList.Axis2),
-          Input.GetJoyAxis(0, (int)JoystickList.Axis3)
-        ).Angle();
-        GD.Print(angle);
-        return angle;
-      }
+	  float GetControllerRotation()
+	  {
+		float angle = new Vector2(
+		  Input.GetJoyAxis(0, (int)JoystickList.Axis2),
+		  Input.GetJoyAxis(0, (int)JoystickList.Axis3)
+		).Angle();
+		GD.Print(angle);
+		return angle;
+	  }
 
-      return OptionsManager.Options.ControllerMode == OptionsData.ControllerModeE.Mouse
-          // Mouse aiming
-          ? GetMouseRotation()
+	  return OptionsManager.Options.ControllerMode == OptionsData.ControllerModeE.Mouse
+		  // Mouse aiming
+		  ? GetMouseRotation()
 
-          // Controller aiming
-          : GetControllerRotation();
-    }
+		  // Controller aiming
+		  : GetControllerRotation();
+	}
   }
 }
