cff41eb814aa0d4b7329ce22f6744b77467b0a2a
Kishan Patel <bishan.batel@protonmail.com>
2021-09-16 22:06:35 -0500

    Added basic sound system / events

--- src/managers/sound/sfx/Sfx2D.cs
+++ src/managers/sound/sfx/Sfx2D.cs
@@ -1,21 +1,9 @@
 using Godot;
-using System;
 
-public class SFX2D : AudioStreamPlayer2D
+namespace Nita.managers.sound.sfx
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    // TODO add randomization for sound
+    public class Sfx2D : AudioStreamPlayer2D
     {
-        
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
