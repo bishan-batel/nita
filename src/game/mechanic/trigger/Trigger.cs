d314ae8d53942271326a9895f255a748448071c9
Kishan Patel <bishan.batel@protonmail.com>
2021-09-18 23:01:25 -0500

    Added MusicSoundPlayer and added more signals (mainly for sound based events)

--- src/game/mechanic/trigger/Trigger.cs
+++ src/game/mechanic/trigger/Trigger.cs
@@ -1,21 +1,12 @@
 using Godot;
-using System;
 
-public class Trigger : Node
+namespace Nita.game.mechanic.trigger
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    // TODO make actual trigger
+    public class Trigger : Node
     {
-        
+        public override void _Ready()
+        {
+        }
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
--- src/managers/sound/music/MusicPlayer.cs
+++ src/managers/sound/music/MusicPlayer.cs
@@ -1,21 +1,82 @@
-using Godot;
 using System;
+using GDMechanic.Wiring;
+using Godot;
+using Nita.addons.godotrx;
+using Nita.utils;
+using Object = Godot.Object;
 
-public class MusicPlayer : AudioStreamPlayer
+namespace Nita.managers.sound.music
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+  public class MusicPlayer : AudioStreamPlayer
+  {
+    public const float MinDb = -60;
 
-    // Called when the node enters the scene tree for the first time.
+    [Export(PropertyHint.None, "Seconds until player starts")]
+    public float AutoplayOffsetStart { set; get; }
+
+    [Export] public new bool Autoplay { set; get; }
+
+    // TODO fix fade in curves
+    // [Export] public Curve FadeInCurve { set; get; }
+    [Export] public float FadeInDuration, SoundStartPosition;
+
+    [Export(PropertyHint.Range, "-60, 24")]
+    public new float VolumeDb { set; get; }
+
     public override void _Ready()
     {
-        
+      this.Wire();
+      // FadeInCurve = new Curve();
+      base.Autoplay = false;
+
+      // Autoplay functionality 
+      if (!Autoplay) return;
+
+
+      if (AutoplayOffsetStart != 0f)
+      {
+        this.Dispatch(FadeIn, AutoplayOffsetStart);
+        return;
+      }
+
+      FadeIn();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    // For connecting with signals
+    // ReSharper disable once InconsistentNaming
+    public void trigger(params object[] rofl) => FadeIn();
+
+    // ReSharper disable once InconsistentNaming
+    public void trigger() => FadeIn();
+
+    public void FadeIn()
+    {
+      base.VolumeDb = MinDb;
+      float time = 0;
+
+      EmitSignal(nameof(OnPlay));
+      Play();
+
+      IDisposable disposable = this.OnProcess().Subscribe(delta =>
+      {
+        time += delta;
+        float percentComplete = time / FadeInDuration;
+
+        // float interpolatedCurve = _curveValAt(percentComplete);
+        // GD.Print($"{percentComplete}% ic: {interpolatedCurve} t {FadeInCurve.Interpolate(1f)}");
+
+        float curvedDb = Mathf.Lerp(MinDb, VolumeDb, percentComplete);
+        base.VolumeDb = Mathf.Min(curvedDb, FadeInDuration);
+      });
+
+      this.Dispatch(() =>
+      {
+        disposable.Dispose();
+        base.VolumeDb = VolumeDb;
+      }, FadeInDuration);
+    }
+
+    [Signal]
+    public delegate void OnPlay();
+  }
 }
